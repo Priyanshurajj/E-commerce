9e2e587d5f64929f10c9235f1a2a037260c365cf
Priyanshu_raj <rpriyanshu758@gmail.com>
2022-07-04 22:17:08 +0530

    first commit

---
+++ src/constants/images.js
@@ -0,0 +1,44 @@
+import i1 from '../assets/5.jpg';
+import i2 from '../assets/6.jpg';
+import i3 from '../assets/7.jpg';
+import i4 from '../assets/8.jpg';
+import i5 from '../assets/9.jpg';
+import i6 from '../assets/10.jpg';
+import i7 from '../assets/11.jpg';
+import i8 from '../assets/12.jpg';
+import i9 from '../assets/13.jpg';
+import i10 from '../assets/14.jpg';
+import i11 from '../assets/15.jpg';
+import bg from '../assets/bg.png';
+import menu from '../assets/menu.png';
+import overlaybg from '../assets/overlaybg.png';
+import spoon from '../assets/spoon.svg';
+import welcome from '../assets/1.jpg';
+import findus from '../assets/findus.png';
+import sign from '../assets/signature.png';
+import quote from '../assets/quote.png';
+import woodkart from '../assets/woodkart.png';
+import about from '../assets/16.jpg';
+export default {
+  bg,
+  menu,
+  overlaybg,
+  spoon,
+  welcome,
+  findus,
+  sign,
+  quote,
+  woodkart,
+  about,
+  i1,
+  i2,
+  i3,
+  i4,
+  i5,
+  i6,
+  i7,
+  i8,
+  i9,
+  i10,
+  i11,
+};
---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {images} from '../../constants';
+
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about-sponn" className="spoon__img"/>
+          <p className="p__opensans">What started small,with a single small discount store and a simple idea of sell more for less has grown over the last 50 years as one of the biggest E-commerce plateform of world.Each week nearly 12 million customers and members visit approximately 11,500 stores under 56 banners in 27 countries.</p>
+        <button type="button" className="custom__button">View More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.about} alt="about img"/>
+
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about-sponn" className="spoon__img"/>
+        <p className="p__opensans">WoodKart employs over 2.2 million associates worldwide. WoodKart continues to be a leader in sustainability, corporate philanthropy and employment opportunity.It's all part of our unwavering commitment to creating opportunities and bringing value to customers and communities around the world.</p>
+        <button type="button" className="custom__button">View More</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
